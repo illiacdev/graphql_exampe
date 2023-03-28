37cb441d76630291bad113ac3fb538df246efa77
illiacdev <illiacdev@gmail.com>
2023-03-28 17:41:34 +0900

    ..

--- back_end/src/main/java/com/example/back_end/domain/base/ExtraInfo.java
+++ back_end/src/main/java/com/example/back_end/domain/base/ExtraInfo.java
@@ -2,26 +2,18 @@ package com.example.back_end.domain.base;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-public class Info {
+public class ExtraInfo {
     @Id
     String name;
     String value;
-
-    @Builder.Default
-    @OneToMany
-    List<ExtraInfo> extraInfos = new ArrayList<>();
 }
